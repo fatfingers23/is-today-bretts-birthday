5fe03d21dab907a64b726520c3df350d9e644946
Bailey Townsend <baileytownsend2323@gmail.com>
2022-10-26 09:56:15 -0500

    The project is done

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,26 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+let today = new Date();
+let theDay = 26;
+let theMonth = 10;
+const todaysDay = today.getDate();
+const todaysMonth = today.getMonth() + 1;
+
+const isTodayBrettsBirthday = (theDay === todaysDay) && (theMonth === todaysMonth);
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+<main>
+  <div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold text-white">{{isTodayBrettsBirthday ? 'YES' : 'NO'}}</h1>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
+</main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
